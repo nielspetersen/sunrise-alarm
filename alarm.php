9f4e268af08e3c79275d317b99378ce036a83c2c
Niels Petersen <niels.p@live.de>
2020-04-28 18:14:34 +0200

    Update form for updating alarms and fix for bug while writing json file

--- alarm.php
+++ alarm.php
@@ -1,9 +1,23 @@
 <?php
-$wakeuptime = $_POST['wakeup-time'];
-// Remove digits for seconds and miliseconds
-$wakeuptime = substr($wakeuptime, 0, 16);
+$days = ['monday', 'tuesday', 'wednesdays', 'thursday', 'friday', 'saturday', 'sunday'];
 
-echo 'The wake-up time ' . $wakeuptime . ' .';
+$alarms = ["alarms" => []];
 
-file_put_contents('data/alarm_time.txt', $wakeuptime);
+foreach($days as $day) {
+    $alarm = [
+            "time_utc" => $_POST[$day . '_alarmtime'],
+            "melody" => "Default",
+            "advance_start" => $_POST[$day . '_advance_time'],
+            "active" => ($_POST[$day . '_active'] == 'active' ? true : false)
+    ];
+    
+    $alarms['alarms'][$day] = $alarm;
+}
+
+$json_content = json_encode($alarms, JSON_PRETTY_PRINT);
+$json_content = str_replace(['[', ']'], '', $json_content); # Remove square brackets
+
+file_put_contents('data/alarms.json', $json_content);
+
+header("Location: index.php");
 ?>
---
+++ index.php
@@ -0,0 +1,45 @@
+<!doctype html> <html lang="en"> 
+<head> 
+	<meta charset="utf-8"/> 
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+	<title>Set sunrise alarm</title>
+	<link rel="stylesheet" type="text/css" href="assets/main.css">
+</head>
+<body>
+	<div class="content">
+		<h1>Sunrise Alarm</h1>
+		<h3>Upcomping alarms</h3>
+		<span style="font-weight:bold;">Please set your wake-up time:</span>
+
+		<?php 	
+			$content = file_get_contents("data/alarms.json");
+			$json_content = json_decode($content, true);
+		?>
+		<div id="alarms">
+			<form id="addAlarm" action="alarm.php" method="post">
+				<button type="submit" name="set-alarm">Save alarms</button>	
+				<table id="alarm-table">
+					<th>Day</th>
+					<th>Status</th>
+					<th>Time</th>
+					<th>Advance Start (in Min)</th>
+					<?php foreach($json_content['alarms'] as $key => $alarm) {
+						echo '<tr>';
+						echo '<td><b>', ucfirst($key), '</b></td>';
+						echo '<td><input type="checkbox" id="', $key, '_active" name="', $key, '_active" value="active" ', ($alarm['active'] == true) ? 'checked=checked' : '', '></td>'; 
+						echo '<td><input type="time" id="', $key, '_alarmtime" name="', $key, '_alarmtime" value=', $alarm['time_utc'], '></td>';
+						echo '<td><select id="', $key, '_advance_time" name="', $key, '_advance_time">';
+							echo '<option value="5" ', ($alarm['advance_start'] == 5) ? 'selected=selected' : '', '>5 Minutes</option>';
+							echo '<option value="10" ', ($alarm['advance_start'] == 10) ? 'selected=selected' : '', '>10 Minutes</option>';
+							echo '<option value="15" ', ($alarm['advance_start'] == 15) ? 'selected=selected' : '', '>15 Minutes</option>';
+							echo '<option value="20" ', ($alarm['advance_start'] == 20) ? 'selected=selected' : '', '>20 Minutes</option>';
+							echo '<option value="30" ', ($alarm['advance_start'] == 30) ? 'selected=selected' : '', '>30 Minutes</option>';
+						echo '</select></td>';
+						echo '</tr>';
+					}
+					?>
+				</table>
+			</form>
+		</div>
+	</div>
+</body>
